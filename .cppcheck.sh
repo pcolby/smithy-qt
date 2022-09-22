e28a2ce6540298a1449fd14cc1fc2485d9f3afac
Paul Colby <git@colby.id.au>
2022-09-23 09:26:10 +1000

    Use a more-recent Ubuntu version for cppcheck
    Specifically, the current `ubuntu-lates` (20.04) has a very old,
    buggy version of cppcheck (1.90) which is reports false failures.

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: 2013-2022 Paul Colby <git@colby.id.au>
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
+cppcheck --version
 for platform in native unix32 unix64 win32A win32W win64 unspecified; do
   for defines in -{D,U}QTSMITHY_LIBRARY\ -{DPROJECT_PRE_RELEASE=pre,UPROJECT_PRE_RELEASE}; do
     echo "$platform $defines"
