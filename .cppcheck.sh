850d21a92ca4701927661dbb77441ff186b99e58
Paul Colby <git@colby.id.au>
2022-09-22 22:48:20 +1000

    Add local .cppcheck.sh script

---
+++ .cppcheck.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2013-2022 Paul Colby <git@colby.id.au>
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+for platform in native unix32 unix64 win32A win32W win64 unspecified; do
+  for defines in -{D,U}QTSMITHY_LIBRARY\ -{DPROJECT_PRE_RELEASE=pre,UPROJECT_PRE_RELEASE}; do
+    echo "$platform $defines"
+    cppcheck $defines --enable=all --error-exitcode=1 --force \
+      --language=c++ --platform=$platform --std=c++{03,11,14,17,20} \
+      --suppress={missingInclude,unknownMacro}  \
+      --suppress=unusedFunction:test/unit/*.cpp \
+      --quiet "$@" src test
+  done
+done
